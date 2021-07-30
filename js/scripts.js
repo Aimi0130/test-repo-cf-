ecbf10c6eb6a30c577f0874c23807ab26e3a223c
Aimi0130 <amour5602@yahoo.co.jp>
2021-07-30 16:15:14 +0900

    Updated script.js

--- js/scripts.js
+++ js/scripts.js
@@ -2,3 +2,12 @@
     console.log("Welcome! Hope you enjoy my portfolio!")
     function menuToggle() {
     }
+
+    function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+}
+
+function menuToggle() {
+  var x = document.getElementById('myNavtoggle');
+  x.className += ' responsive';
+}
