dbe901d0f1b38af0379512c859be95a5cdf384b2
Aimi0130 <amour5602@yahoo.co.jp>
2021-07-30 16:16:45 +0900

    Updated script.js

--- js/scripts.js
+++ js/scripts.js
@@ -3,11 +3,12 @@
     function menuToggle() {
     }
 
-    function menuToggle() {
-  var x = document.getElementById('myNavtoggle');
-}
 
 function menuToggle() {
   var x = document.getElementById('myNavtoggle');
-  x.className += ' responsive';
+  if (x.className === 'navtoggle') {
+    x.className += ' responsive';
+  } else {
+    x.className = 'navtoggle';
+  }
 }
